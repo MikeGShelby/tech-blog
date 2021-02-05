c17b0d9d94f40f1f0a4f27259830c1ccd161e316
Mike Shelby <mikegshelby@gmail.com>
2021-02-05 14:10:39 -0600

    added styles to header

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,14 @@
 module.exports = {
     format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+      const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+
+    const d = new Date(date);
+    // document.write("The current month is " + monthNames[d.getMonth()]);
+
+    return `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+
+      // return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
     format_plural: (word, amount) => {
         if (amount !== 1) {
